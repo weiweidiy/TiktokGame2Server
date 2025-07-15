4c528f1703d6f78a5dfa4e2f98749b882d1f227c
Jichunwei <49595272@qq.com>
2025-07-15 18:54:16 +0800

    update

---
+++ TiktokGame2Server/TiktokGame2Server/Entities/MyDbContext.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TiktokGame2Server.Entities
+{
+    public class MyDbContext(DbContextOptions<MyDbContext> options) : DbContext(options)
+    {
+    }
+}
