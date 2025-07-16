47cc8d413c7fcec1a723e84feaba3d34ace46b33
Jichunwei <49595272@qq.com>
2025-07-16 13:47:35 +0800

    refactor

---
+++ TiktokGame2Server/TiktokGame2Server/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tiktok;
+
+namespace TiktokGame2Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        [HttpPost("FastLogin")]
+        public virtual ActionResult<LoginDTO> FastLogin(string uid)
+        {
+            var loginDTO = new LoginDTO();
+            var user = new UserDTO();
+            loginDTO.User = user;
+
+            return loginDTO;
+        }
+
+
+    }
+
+
+}
